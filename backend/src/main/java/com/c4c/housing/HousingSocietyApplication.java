a40a7b5e6f9eaec79d5f12af7102ca082f540a71
sheel.prabhakar <sheel.prabhakar@ukg.com>
2023-12-27 12:29:16 +0530

    added initial code

---
+++ backend/src/main/java/com/c4c/housing/HousingSocietyApplication.java
@@ -0,0 +1,13 @@
+package com.c4c.housing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HousingSocietyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HousingSocietyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/c4c/housing/HousingSocietyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4c.housing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingSocietyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
