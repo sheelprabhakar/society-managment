6bdabf6e9ba98fdbd93331bf896a9d5d72a75381
Sheel Sindhu Prabhakar <sheel.prabhakar@gmail.com>
2024-01-21 23:08:05 +0530

    added memcached support

---
+++ backend/src/main/java/com/c4c/housing/config/CachingConfig.java
@@ -0,0 +1,12 @@
+package com.c4c.housing.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+}
--- backend/src/main/java/com/c4c/housing/core/service/impl/UserTokenServiceImpl.java
+++ backend/src/main/java/com/c4c/housing/core/service/impl/UserTokenServiceImpl.java
@@ -4,6 +4,8 @@ import com.c4c.housing.core.entity.UserTokenEntity;
 import com.c4c.housing.core.repository.UserTokenRepository;
 import com.c4c.housing.core.service.UserTokenService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.Calendar;
@@ -19,11 +21,13 @@ public class UserTokenServiceImpl implements UserTokenService {
     }
 
     @Override
+    @Cacheable(value = "tokens",key = "#id.toString()")
     public UserTokenEntity getById(UUID id){
         return this.userTokenRepository.findById(id).orElse(null);
     }
 
     @Override
+    @CacheEvict(value = "tokens", key = "#id.toString()")
     public UserTokenEntity update(UUID id, String token){
         UserTokenEntity tokenEntity = this.userTokenRepository.findById(id).orElse(null);
         if(tokenEntity == null){
